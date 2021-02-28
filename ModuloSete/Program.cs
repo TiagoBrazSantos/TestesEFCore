d6fba9c9b1a8e03706492d6adaa60b6dc7003f69
Tiago Braz <tiagobrazsantos@outlook.com>
2021-02-28 15:11:50 -0300

    Adicionar módulo 7.

---
+++ ModuloSete/Program.cs
@@ -0,0 +1,12 @@
+﻿using Curso.Modulos;
+
+namespace EstudosEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModuloSete.ConsultarDepartamentos();
+        }
+    }
+}
